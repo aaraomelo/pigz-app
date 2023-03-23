f374954d00fbaf604d308dea58e1f9b5d4bfbc74
Aarão Melo Lopes <aarao.melo.lopes@gmail.com>
2023-03-23 11:56:34 -0400

    feat: Redux library configuration

---
+++ src/Store/index.js
@@ -0,0 +1,33 @@
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
+import { getPersistConfig } from "redux-deep-persist";
+import storage from "redux-persist/lib/storage";
+import { Provider } from "react-redux";
+
+const reducer = combineReducers({});
+
+const persistConfig = getPersistConfig({
+    key: "root",
+    storage,
+    blacklist: [],
+    rootReducer: reducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, reducer);
+
+const store = configureStore({
+    reducer: persistedReducer,
+});
+
+const persistor = persistStore(store);
+
+export default function Store({ children }) {
+    return (
+        <Provider store={store}>
+            <PersistGate loading={null} persistor={persistor}>
+                {children}
+            </PersistGate>
+        </Provider>
+    )
+};
