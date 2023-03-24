7031493849da606d5bfd126f982d7ad415816e2b
Aarão Melo Lopes <aarao.melo.lopes@gmail.com>
2023-03-24 17:14:50 -0400

    feat: validate field reducer function

--- src/Store/form/reducer.js
+++ src/Store/form/reducer.js
@@ -3,6 +3,7 @@ import { FormInitialState as initialState } from "./state";
 import * as actionTypes from "./actionTypes";
 import { proxySchema } from "../proxy";
 import { unmask } from "../masks";
+import validations from "../validations";
 
 const setFormField = createAction(actionTypes.SET_FORM_FIELD);
 
@@ -10,9 +11,19 @@ export default createReducer(initialState,
   (builder) => {
     builder
       .addCase(setFormField, (state, { payload: { pointer, value } }) => {
-        const schema = proxySchema(state, pointer);
-        const { mask = 'default' } = schema;
-        schema.state = unmask[mask](value);
+        const field = proxySchema(state, pointer);
+        field.state = unmask[field.mask ?? 'default'](value);
+        validateField(field);
       })
   }
 );
+
+export const validateField = (field) => {
+  if (!field?.validation) return;
+  field.blur = true;
+  field.validator = [];
+  Object.keys(field.validation).forEach((name) => {
+    !validations[name](field.state, field.validation[name]) &&
+      field.validator.push(`form.error.validation.${name}`);
+  });
+}
--- src/Store/messages/state.js
+++ src/Store/messages/state.js
@@ -1,18 +1,18 @@
 export const MessagesInitialState = {
   "pt": {
-    "form.error.required": "Este campo é necessário",
-    "form.error.pattern": "Este texto não corresponde com o padrão esperado"
+    "form.error.validation.required": "Este campo é necessário",
+    "form.error.validation.pattern": "Este texto não corresponde com o padrão esperado"
   },
   "es": {
-    "form.error.required": "Este campo es requerido",
-    "form.error.pattern": "Este texto no coincide con el patrón esperado"
+    "form.error.validation.required": "Este campo es requerido",
+    "form.error.validation.pattern": "Este texto no coincide con el patrón esperado"
   },
   "en": {
-    "form.error.required": "This field is required",
-    "form.error.pattern": "This text does not match the expected pattern"
+    "form.error.validation.required": "This field is required",
+    "form.error.validation.pattern": "This text does not match the expected pattern"
   },
   "ar": {
-    "form.error.required": "هذا الحقل مطلوب",
-    "form.error.pattern": "هذا النص لا يتطابق مع النمط المتوقع"
+    "form.error.validation.required": "هذا الحقل مطلوب",
+    "form.error.validation.pattern": "هذا النص لا يتطابق مع النمط المتوقع"
   }
 };
