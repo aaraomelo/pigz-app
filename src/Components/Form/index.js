611dced7f76fe9d95e4675dc72a36720fb9d3607
Aarão Melo Lopes <aarao.melo.lopes@gmail.com>
2023-03-24 09:48:51 -0400

    feat: form header implementation

--- src/Components/Form/index.js
+++ src/Components/Form/index.js
@@ -2,8 +2,11 @@ import FormAction from "./FormAction";
 import FormControl from "./FormControl";
 import FormDropdown from "./FormDropdown";
 import FormField from "./FormField";
+import FormHeader from "./FormHeader";
 import FormInput from "./FormInput";
 import FormObject from "./FormObject";
+import FormSubtitle from "./FormSubtitle";
+import FormTitle from "./FormTitle";
 
 export default Object.assign(
   ({ children, className }) => (
@@ -23,5 +26,8 @@ export default Object.assign(
     Input: FormInput,
     Dropdown: FormDropdown,
     Action: FormAction,
+    Title: FormTitle,
+    Subtitle: FormSubtitle,
+    Header: FormHeader,
   }
 );
